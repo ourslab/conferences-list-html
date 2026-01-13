74f81e8f382dd507e7aee7393bd7bb31aa48a952
u2key <248891400+u2key@users.noreply.github.com>
2026-01-14 01:35:43 +0900

    Update data.js

--- data.js
+++ data.js
@@ -12,6 +12,7 @@ let conferences_list = [];
 if (typeof(conferences_list) !== 'undefined') conferences_list.push({
   type    : "international", // "international" or "domestic"
   name    : {text: "FPT 2025 (conference track)", url: "https://fpt2025.shanghaitech.edu.cn/"},
+  tag     : "FPT",
   apply   : {date: {year: 2025, month: 7, day: 20}},
   abstract: {date: {year: 2025, month: 7, day: 20}},
   paper   : {date: {year: 2025, month: 8, day: 1}},
@@ -22,6 +23,7 @@ if (typeof(conferences_list) !== 'undefined') conferences_list.push({
 if (typeof(conferences_list) !== 'undefined') conferences_list.push({
   type    : "domestic", // "international" or "domestic"
   name    : {text: "DesignGaia 2025", url: "https://www.ieice.org/iss/reconf/top/"},
+  tag     : "DesignGaia",
   apply   : {date: {year: 2025, month: 9, day: 3}},
   abstract: {},
   paper   : {date: {year: 2025, month: 10, day: 0}},
